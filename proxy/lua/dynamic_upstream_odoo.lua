fd992a26a33b4b839f9800badeea9fadec7ce90c
Zebroo <odoo@zebroo.de>
2025-05-16 05:27:36 +0000

    fix behind proxy

--- proxy/lua/dynamic_upstream_odoo.lua
+++ proxy/lua/dynamic_upstream_odoo.lua
@@ -1,3 +1,8 @@
+local incoming_headers = ngx.req.get_headers()
+local request_proto = incoming_headers["X-Forwarded-Proto"] or "http"  -- default fallback
+ngx.req.set_header("X-Forwarded-Proto", request_proto)
+
+
 local myngx = require("myngx")
 local projectname = os.getenv("PROJECT_NAME")
 local hostname = "odoo"
