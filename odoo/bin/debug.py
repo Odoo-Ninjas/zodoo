411a21827bf843fcb250d03273be88b28db873c1
Marc Wimmer <marc@zebroo.de>
2025-06-19 20:56:20 +0200

    fix return code

--- odoo/bin/debug.py
+++ odoo/bin/debug.py
@@ -57,7 +57,8 @@ class Debugger(object):
         os.chdir(self.odoolib_path)
         if not cmd[0].startswith("/"):
             cmd = ["python3"] + cmd
-        res = not subprocess.run(cmd, cwd=self.odoolib_path)  # exitcode
+        proc = subprocess.run(cmd, cwd=self.odoolib_path)  # exitcode
+        res = proc.returncode == 0
         sane_tty()
         return res
 
