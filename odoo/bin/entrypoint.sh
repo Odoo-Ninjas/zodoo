d8d69c5889e994f94822fda2c6580d5e7df36b9a
cicd_ipe <cicd_ipe>
2025-07-03 08:56:35 +0000

    fix start container

--- odoo/bin/entrypoint.sh
+++ odoo/bin/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-path="/usr/local/bin/wodoo_python"
-export CUSTOMS_DIR=/opt/src
-if [[ ! -e "$path" ]]; then
-	tee -a "$path" > /dev/null <<- EOT
-		#!/bin/bash
-		cd $(dirname "$WODOO_PYTHON")
-		./python3 $( echo '"$@"' )
-	EOT
-	chmod a+x "$path"
-fi
+# path="/usr/local/bin/wodoo_python"
+# export CUSTOMS_DIR=/opt/src
+# if [[ ! -e "$path" ]]; then
+# 	tee -a "$path" > /dev/null <<- EOT
+# 		#!/bin/bash
+# 		cd $(dirname "$WODOO_PYTHON")
+# 		./python3 $( echo '"$@"' )
+# 	EOT
+# 	chmod a+x "$path"
+# fi
+set -x
 exec "$WODOO_PYTHON" /odoolib/entrypoint.py "$@"
