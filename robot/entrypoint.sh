b63c808df49a105c5e69c272ae45bd297eae0ce3
Marc Wimmer <marc@zebroo.de>
2025-07-10 23:32:02 +0200

    robot rights

--- robot/entrypoint.sh
+++ robot/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 tee >/tmp/archive <&0
 
-echo "Fixing possible wrong user rights"
-#find /opt/src -not -user robot -exec chown robot {} \;
-#find /opt/robot/.odoo -not -user robot -exec chown robot {} \;
-#find /opt/robot/.odoo/images -not -user robot -exec chown robot {} \;
-echo "Finished fixxing possible missed ownerships"
 
 export USERNAME=robot
 chmod a+rw -R "$ROBO_UPLOAD_FILES_DIR_LOCAL"
+chmod a+rw -R /opt/output
 exec gosu $USERNAME python3 robotest.py "$@"
