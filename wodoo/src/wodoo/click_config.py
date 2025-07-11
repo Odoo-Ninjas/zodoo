b9e98d8837c838a061401a8c28480882923c949d
Marc Wimmer <marc@zebroo.de>
2025-07-11 15:11:31 +0200

    Updating submodule wodoo/src

--- wodoo/src/wodoo/click_config.py
+++ wodoo/src/wodoo/click_config.py
@@ -28,6 +28,8 @@ class Config(object):
     ):
         from .consts import YAML_VERSION
 
+        if project_name is None:
+            project_name = os.environ.get("project_name", None)
         from . import odoo_config  # NOQA
 
         from .tools import _get_customs_root
