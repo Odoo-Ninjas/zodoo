5ad16c4ef2bfc9b0bd5c9dc0531075dae62670f3
Marc Wimmer <marc@zebroo.de>
2025-07-22 00:11:43 +0200

    change owner

--- odoo/bin/entrypoint.py
+++ odoo/bin/entrypoint.py
@@ -14,6 +14,8 @@ os.system(f"chown '{owner}:{owner}' /opt/files")
 # important is especially the .config folder, so that libreoffice works
 #print(f"Setting ownership of /home/odoo to {owner}")
 os.system(f"chown '{owner}:{owner}' /home/odoo")  # -R too heavy
+# CICD compatibility TODO make nicer
+os.system(f"chown '{owner}:{owner}' -R /opt/src_cicd_modules")
 os.system("git config --global --add safe.directory /opt/src")
 
 cmd, args = None, None
