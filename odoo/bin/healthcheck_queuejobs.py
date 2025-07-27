28c5555eaed8646708dacb9240acfa2be7abeffe
Marc Wimmer <marc@zebroo.de>
2025-07-27 10:02:09 +0200

    queuejob restart

--- odoo/bin/healthcheck_queuejobs.py
+++ odoo/bin/healthcheck_queuejobs.py
@@ -2,6 +2,7 @@ import psycopg2
 import os
 import datetime
 
+
 def check_queue_job_status(db_config):
     """
     Checks if there are any pending jobs and no running jobs in the queue_job table.
@@ -9,6 +10,8 @@ def check_queue_job_status(db_config):
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     query_pending = "SELECT COUNT(*) FROM queue_job WHERE state in ('pending', 'enqueued') and (eta is null or eta < '{now}');"
     query_running = "SELECT COUNT(*) FROM queue_job WHERE state = 'started';"
+
+    QUEUEJOBS_MAX_AGE_BEFORE_RESTART = 60 * int(os.getenv("QUEUEJOBS_MAX_AGE_BEFORE_RESTART_MINUTES", "120"))
 
     try:
         conn = psycopg2.connect(**db_config)
@@ -24,7 +27,13 @@ def check_queue_job_status(db_config):
             raise Exception("Pending jobs detected with no running jobs.")
         else:
             print(f"ℹ️ Pending: {pending_count}, Running: {running_count}")
-            return False
+
+        cursor.execute("SELECT MIN(date_started) FROM queue_job WHERE state = 'started';")
+        started = cursor.fetchone()
+        if started:
+            started = started[0]
+            if (datetime.now() - started[0]).total_seconds() > QUEUEJOBS_MAX_AGE_BEFORE_RESTART:
+                raise Exception(f"Queue jobs have been running for longer than QUEUEJOBS_MAX_AGE_BEFORE_RESTART_MINUTES= {QUEUEJOBS_MAX_AGE_BEFORE_RESTART}seconds, consider restarting the service.")
 
     finally:
         if cursor:
