5ed08ba5884d1d941c19052e77098f50fd35ba14
cicd <cicd>
2025-07-31 10:46:44 +0200

    Updating submodule wodoo/src

---
+++ proxy/lua/dynamic_upstream_seleniumxpra.lua
@@ -0,0 +1,14 @@
+local myngx = require("myngx")
+local hostname = "selenium_xpra"
+local port = "5900"
+local varname = "target_selenium_xpra"
+
+ngx.log(ngx.INFO, "Redirecting to selenium_xpra")
+
+if os.getenv("RUN_ROBOT") == "1" then
+    local ip = myngx.get_ip(hostname)
+    if ip then
+        ngx.var[varname] = "http://" .. ip .. ":" .. port
+    end
+end
+
---
+++ selenium_xpra/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+fluxbox & 
+google-chrome --no-sandbox --disable-dev-shm-usage --remote-debugging-port=4444
