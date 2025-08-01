bd0cc77a2ec5f3572f574b79e5915737398bb5d1
Marc Wimmer <marc@zebroo.de>
2025-08-01 23:04:03 +0200

    [selenium_customized]

--- selenium_customized/start.sh
+++ selenium_customized/start.sh
@@ -6,8 +6,7 @@ geckodriver --host 0.0.0.0 --port 4444 --allow-hosts=* &
 python3 /usr/local/bin/maximize_all_windows.py &
 
 
-wmctrl -m
-sleep 3
-echo "Starting geckodriver test..."
-python3 /opt/test_geckodriver.py
+# sleep 3
+# echo "Starting geckodriver test..."
+# python3 /opt/test_geckodriver.py
 wait
