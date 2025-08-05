b6273a08cbef37548e1fe906298a2452a53477cf
bjbschroeder <b.j.b.schroeder@googlemail.com>
2025-08-05 15:15:18 +0000

    .

--- selenium_customized/start.sh
+++ selenium_customized/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 wmctrl -m
-geckodriver --host 0.0.0.0 --port 4444 --allow-hosts=* &
+/bin/bash /usr/local/bin/geckodriver_loop.sh &
 python3 /usr/local/bin/maximize_all_windows.py &
 
 
-sleep 3
-echo "Starting geckodriver test..."
-python3 /opt/test_geckodriver.py &
-sleep 5
-pkill -9 -f firefox || true
+# sleep 3
+# echo "Starting geckodriver test..."
+# python3 /opt/test_geckodriver.py &
+# sleep 5
+# pkill -9 -f firefox || true
 wait
