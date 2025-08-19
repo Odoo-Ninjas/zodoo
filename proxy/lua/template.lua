10f6d9bfe597e5d0fe1015104c883845201ad541
Marc Wimmer <marc@zebroo.de>
2025-08-19 13:17:20 +0200

    [conditional_proxy]

--- proxy/lua/template.lua
+++ proxy/lua/template.lua
@@ -11,11 +11,7 @@ local port = "{port}"
 local ip = myngx.get_ip(hostname)
 
 -- If hostname is already a URL, just use it as-is
-if string.sub(hostname, 1, 7) == "http://" or string.sub(hostname, 1, 8) == "https://" then
-    ngx.var['backend'] = hostname
-else
-    local ip = myngx.get_ip(hostname)
-    if ip then
-        ngx.var['backend'] = "http://" .. ip .. ":" .. port
-    end
+local ip = myngx.get_ip(hostname)
+if ip then
+    ngx.var['backend'] = "http://" .. ip .. ":" .. port
 end
