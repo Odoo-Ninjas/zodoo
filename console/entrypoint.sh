1e79b8e74733517b4f4aceb1528bf06ff31ae307
Marc Wimmer <marc@zebroo.de>
2025-09-26 17:55:28 +0200

    console can execute docker

--- console/entrypoint.sh
+++ console/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+/usr/bin/set_docker_group.sh
 usermod -u "${OWNER_UID}" odoo
-usermod -aG "$(stat -c '%G' "/var/run/docker.sock")" odoo
+usermod -aG "${DOCKER_GID}" odoo
 echo "export project_name=$project_name" > /home/odoo/env
 echo "export PROJECT_NAME=$project_name" > /home/odoo/env
 echo "DB_HOST=$DB_HOST" >> /home/odoo/env
