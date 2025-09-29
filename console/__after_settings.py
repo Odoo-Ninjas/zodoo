83e550d4e2adef6526d3497ca33e4f887c620822
Marc Wimmer <marc@zebroo.de>
2025-09-29 11:12:40 +0200

    formatting

--- console/__after_settings.py
+++ console/__after_settings.py
@@ -1,14 +1,14 @@
 import string
 import secrets
 
+
 def generate_password(length=12):
     # Characters: letters, digits, and punctuation
     alphabet = string.ascii_letters + string.digits
     # Use secrets.choice for cryptographically strong randomness
-    return ''.join(secrets.choice(alphabet) for _ in range(length))
-
+    return "".join(secrets.choice(alphabet) for _ in range(length))
 
 
 def after_settings(settings, config):
-    if not settings['CONSOLE_PASSWORD'] and settings['DEVMODE'] != '1':
-        settings['CONSOLE_PASSWORD'] = generate_password(12)
+    if not settings["CONSOLE_PASSWORD"] and settings["DEVMODE"] != "1":
+        settings["CONSOLE_PASSWORD"] = generate_password(12)
