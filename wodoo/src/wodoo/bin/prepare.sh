6f709b238c91a8bd7bc3e1363a5433648d150aee
Marc Wimmer <marc@itewimmer.de>
2026-02-06 12:11:30 +0100

    main: main: Fixed

--- wodoo/src/wodoo/bin/prepare.sh
+++ wodoo/src/wodoo/bin/prepare.sh
@@ -7,14 +7,15 @@ fi
 if [[ "$DEVMODE" != "1" ]]; then
 
 	if [[ "$RUN_POSTGRES" != "1" || -n "$(git status --porcelain)" ]]; then
-		msg="Careful: you seem to debug a non local database!"
-		if [[ "$OSTYPE" == "darwin"* ]]; then
-			osascript -e "display alert \"$msg\""
-		else
-			powershell.exe -Command \
-			"Add-Type -AssemblyName System.Windows.Forms; \
-			[System.Windows.Forms.MessageBox]::Show('$msg')"
-		fi
+	msg="Careful: you seem to debug a non local database!"
+
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		osascript -e "display alert \"$msg\""
+	else
+		powershell.exe -NoProfile -Command \
+		"Add-Type -AssemblyName System.Windows.Forms; \
+		[System.Windows.Forms.MessageBox]::Show(\"$msg\")"
 	fi
+fi
 
 fi
