db2183cd26b96ab1bd303ffa8a07059b8258a84d
Marc Wimmer <marc@itewimmer.de>
2026-02-11 11:15:03 +0100

    main: [main]

--- odoo/bin/entrypoint.sh
+++ odoo/bin/entrypoint.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
-# path="/usr/local/bin/wodoo_python"
-# export CUSTOMS_DIR=/opt/src
-# if [[ ! -e "$path" ]]; then
-# 	tee -a "$path" > /dev/null <<- EOT
-# 		#!/bin/bash
-# 		cd $(dirname "$WODOO_PYTHON")
-# 		./python3 $( echo '"$@"' )
-# 	EOT
-# 	chmod a+x "$path"
-# fi
 set -e
-if [[ -f /opt/venv.tar.gz ]]; then
-	cd /opt
-	tar \
-		--use-compress-program=zstd \
-		--extract \
-		--file=/opt/venv.tar.gz \
-		--preserve-permissions \
-		--same-owner \
-		--xattrs \
-		--acls
-	rm /opt/venv.tar.gz
-fi
-if [[ -f /usr/share.tar.gz ]]; then
-	cd /usr
-	tar \
-		--use-compress-program=zstd \
-		--extract \
-		--file=/usr/share.tar.gz \
-		--preserve-permissions \
-		--same-owner \
-		--xattrs \
-		--acls
-	rm /usr/share.tar.gz
-	ls -lhtra /usr
-fi
+xtract() {
+	local archive="$1"
+	local target_dir="$2"
+
+	if [[ -f "$archive" ]]; then
+		cd "$target_dir"
+		tar -I zstd -xpf "$archive" \
+			--preserve-permissions \
+			--same-owner \
+			--xattrs --xattrs-include='*' \
+			--acls
+		rm -f "$archive"
+	fi
+}
+xtract /opt/venv.tar.gz /opt
+xtract /usr/share.tar.gz /usr
+
 chown $OWNER_UID /home/odoo/.config -R
 exec "$WODOO_PYTHON" /odoolib/entrypoint.py "$@"
