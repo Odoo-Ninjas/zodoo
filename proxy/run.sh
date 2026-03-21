c30ed0c015c2cef8cc831c3e9d16a8b9737ae36d
github-action@github.com <github-action@github.com>
2026-03-21 12:48:10 +0000

    owner change proxy removed

--- proxy/run.sh
+++ proxy/run.sh
@@ -12,7 +12,9 @@ python3 /usr/local/bin/setup_config_files.py || exit 1
 python3 /usr/local/bin/static_webserver.py $CONSTRUCTION_SITE_PORT /opt/construction_site/index.html &
 
 # fix rights;-
-chmod a+r -R "$CONF_DIR" "$LUA_DIR" /usr/local/openresty/nginx/static
-chown nobody:nobody -R "$CONF_DIR" "$LUA_DIR" /usr/local/openresty/nginx/static
+chmod a+r -R "$CONF_DIR" "$LUA_DIR"
+chown nobody:nobody -R "$CONF_DIR" "$LUA_DIR"
+# static is mounted read-only from the host; do not chown/chmod it
+# to avoid changing ownership of host files
 
 /usr/local/openresty/bin/openresty -g 'daemon off;'
