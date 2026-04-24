56d1819b34f4b9be5a4510d7eddaaa6b8bf07278
Marc Wimmer <marc@itewimmer.de>
2026-04-24 23:32:02 +0200

    Revert "optimize/space_usage_odoo_containers"
    This reverts commit d57d6e0c99cca4c0557bc58fd6e3e18f7960193c.

--- zodoo/src/zodoo/lib_control_with_docker.py
+++ zodoo/src/zodoo/lib_control_with_docker.py
@@ -397,16 +397,18 @@ def build(
         platform = subprocess.check_output(
             ["/usr/bin/uname", "-m"], encoding="utf8"
         ).strip()
-    _arch_map = {"x86_64": "amd64", "aarch64": "arm64"}
-    _arch = platform.split("/")[-1]
-    _arch = _arch_map.get(_arch, _arch)
+    # options += ["--platform", platform]
+
+    # if platform:
+    #     import pudb;pudb.set_trace()
+    #     options += ["--platform", platform]
 
     __dc(
         config,
         ["build"] + options + list(machines),
         env={
             "ODOO_VERSION": config.odoo_version,
-            "DOCKER_DEFAULT_PLATFORM": f"linux/{_arch}",
+            "DOCKER_DEFAULT_PLATFORM": f"linux/{platform}",
             "DOCKER_BUILDKIT": "1",
             "COMPOSE_BAKE": "true",
         },
